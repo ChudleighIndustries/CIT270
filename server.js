da565a436dd1ddf063cbe356e5c1b1f418f938a4
ChudleighIndustries <levichud@gmail.com>
2023-01-18 13:43:45 -0700

    added to server.json

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const app = express();
 
 const port = 3000;
 
+const bodyParser = require ('body-parser');
+
+app.use(bodyParser.json()); //This looks for incoming data
+
 app.get("/",(req, res) => {
-    res.send("hello levi");
+    res.send("hello world");
 });
+
+app.post('/login', (req,res) => {
+    const loginUser = req.body.userName;
+    console.log('login username: '+loginUser);
+    res.send('Hello '+ loginUser);
+})
 
 app.listen(port,() => {
     console.log("listening");
 });
+
+
+
+
+
+
+//  http://localhost:3000
+// this is going the be the website needed to see the website on the listening port
