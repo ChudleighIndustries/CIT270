60f0e1600f70b39c2cc9fa0bdd48049abe2c6c60
ChudleighIndustries <levichud@gmail.com>
2023-01-20 13:46:02 -0700

    updated code for tokens and uuid

--- server.js
+++ server.js
@@ -6,16 +6,25 @@ const port = 3000;
 
 const bodyParser = require ('body-parser');
 
+const {v4: uuidv4} = require('uuid');
+
 app.use(bodyParser.json()); //This looks for incoming data
 
 app.get("/",(req, res) => {
-    res.send("hello world");
+    res.send("hello levi");
 });
 
 app.post('/login', (req,res) => {
     const loginUser = req.body.userName;
+    const loginPassword = req.body.password; //Access the password data in the body 
     console.log('login username: '+loginUser);
-    res.send('Hello '+ loginUser);
+    if (loginUser=="levichud@gmail.com" && loginPassword=="P@$$w0rD"){
+        const loginToken = uuidv4().toString();
+        res.send(loginToken);
+    } else {
+    res.send(401); //unauthorized
+    res.send('Inncorrect password for '+loginUser);
+    }
 })
 
 app.listen(port,() => {
