f3f556ddcbdad8179b5df37e391d857e444d6719
ChudleighIndustries <levichud@gmail.com>
2023-02-01 15:28:03 -0700

    updated code

--- public/timer.js
+++ public/timer.js
@@ -19,10 +19,15 @@
         counterbutton = document.getElementById('counterbutton');
         let hash= location.hash;//will include the #
         let hashparts = hash.split("#");
+        const cookie = document.cookie
+        .split(';')
+        .find((row) => row.startsWith('stedicookie-'))
+        ?.split9('=')[1];
+        console.log('cookies', JSON.stringify(cookie));
         if (hashparts.length < 2) {
             window.location="/"; //there is no login token on the url, so they must not have logged in yet, we will help redirect them here
         } else {
-            usertoken = hashparts[1];// the url should look like https://stedi.me/timer.html#4c2286a7-8fdc-47c5-b972-739769554c88
+            usertoken = hashparts[1];
             validateToken();//check if token is expired, if not display the email, if expired send to login
         }
     });
