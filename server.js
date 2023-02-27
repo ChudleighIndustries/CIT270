1ddf3f6f1579ab833bcb4d7b5d454d12fa1e3b5a
ChudleighIndustries <levichud@gmail.com>
2023-02-27 13:41:48 -0700

    updated .pem files in the server.js

--- server.js
+++ server.js
@@ -80,9 +80,9 @@ app.post('/login', async(req, res) =>{
 
 https.createServer(
    {
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.cert'),
-ca:fs.readFileSync('./chain.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/levichudleigh.cit270.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/levichudleigh.cit270.com/cert.pem'),
+ca:fs.readFileSync('/etc/letsencrypt/live/levichudleigh.cit270.com/chain.pem')
 },
 app
 ).listen(port, ()=>{
